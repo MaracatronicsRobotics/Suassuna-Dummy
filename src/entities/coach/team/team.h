3d5c6e48f0f882662387f673302121c106ca5ab5
zsmn <zsmn@cin.ufpe.br>
2022-11-17 21:39:03 -0300

    feat(team): adding Team class and its methods for update and register players

---
+++ src/entities/coach/team/team.h
@@ -0,0 +1,69 @@
+/***
+ * Maracatronics Robotics
+ * Federal University of Pernambuco (UFPE) at Recife
+ * http://www.maracatronics.com/
+ *
+ * This file is part of Armorial project.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ ***/
+
+#ifndef TEAM_H
+#define TEAM_H
+
+/// TODO: check this dependency
+#include <QList>
+
+#include <Armorial/Common/Enums/Color/Color.h>
+
+#include <include/proto/messages.pb.h>
+
+#include <src/entities/player/player.h>
+
+class Team
+{
+public:
+    /*!
+     * \brief Team constructor.
+     * \param teamColor The given team color for this Team instance.
+     */
+    Team(const Common::Enums::Color& teamColor);
+
+    /*!
+     * \brief Add a given Player pointer to this Team class.
+     * \param player The given player pointer.
+     */
+    void addPlayer(Player *player);
+
+    /*!
+     * \return The registered Color for this Team instance.
+     */
+    Common::Enums::Color teamColor() const;
+
+protected:
+    friend class WorldMap;
+
+    /*!
+     * \brief Update a player in the list that matches the RobotIdentifier from the given player.
+     * \param player The given player to update.
+     */
+    void updatePlayer(const Armorial::Robot& playerData);
+
+private:
+    // Internal objects
+    QList<Player*> _players;
+    Common::Enums::Color _teamColor;
+};
+
+#endif // TEAM_H
